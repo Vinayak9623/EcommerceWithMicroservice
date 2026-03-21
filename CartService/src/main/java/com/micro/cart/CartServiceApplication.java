a4adad8f0c97de3279cf7043ac1def8dfeefd25e
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-03-21 15:58:52 +0530

    Enable Service registry and Api gateway

--- CartService/src/main/java/com/micro/cart/CartServiceApplication.java
+++ CartService/src/main/java/com/micro/cart/CartServiceApplication.java
@@ -4,8 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Gateway Server URL")})
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
