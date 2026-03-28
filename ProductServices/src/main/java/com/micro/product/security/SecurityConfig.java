a7d7571f8a443eeb8c231b0e274d544af05a9c94
Vinayak Deshmukh <vinayakd9623@gmail.com>
2026-03-28 11:34:35 +0530

    Api Gateway modified

--- ProductServices/src/main/java/com/micro/product/security/SecurityConfig.java
+++ ProductServices/src/main/java/com/micro/product/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**","/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html").permitAll()
+                                "/swagger-ui.html","/uploads/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
